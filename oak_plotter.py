52c9b4fb88592d4cbd78ba17d617308a5f7ae018
Arif <arif@themascoteers.com>
2024-06-24 17:25:25 +1000

    done

--- oak_plotter.py
+++ oak_plotter.py
@@ -13,19 +13,28 @@ DSS = {
 }
 
 
+def sanitize_df(df):
+    if "algorithm" not in df.columns:
+        df['target_size'] = 0
+        df['algorithm'] = 'all'
+        df['time'] = 0
+        df['selected_features'] = ''
+    return df
+
+
 def plot_oak(source, exclude=None):
     if exclude is None:
         exclude = []
     os.makedirs("saved_figs", exist_ok=True)
     dest = os.path.join("saved_figs","out.png")
     if isinstance(source,str):
-        df = pd.read_csv(source)
+        df = sanitize_df(pd.read_csv(source))
     else:
-        df = [pd.read_csv(loc) for loc in source]
+        df = [sanitize_df(pd.read_csv(loc)) for loc in source]
         df = pd.concat(df, axis=0, ignore_index=True)
 
     df.to_csv(os.path.join("saved_figs","source.split.csv"), index=False)
-    colors = ['#909c86', '#e389b9', '#269658', '#5c1ad6', '#f20a21', '#000000']
+    colors = ['#909c86', '#e389b9', '#269658', '#5c1ad6', '#f20a21', '#45f512']
     markers = ['s', 'P', 'D', '^', 'o', '*', '.']
     labels = ["Overall Accuracy (OA)", "Average Accuracy (AA)", "Cohen's kappa ($\kappa$)"]
 
@@ -48,15 +57,28 @@ def plot_oak(source, exclude=None):
             for algorithm_index, algorithm in enumerate(algorithms):
                 if algorithm in exclude:
                     continue
-                algorithm_counter = algorithm_counter + 1
                 algorithm_label = algorithm
                 if algorithm in ALGS:
                     algorithm_label = ALGS[algorithm]
                 alg_df = dataset_df[dataset_df["algorithm"] == algorithm]
                 alg_df = alg_df.sort_values(by='target_size')
+                linestyle = "-"
+                color = colors[algorithm_counter]
+                marker = markers[algorithm_counter]
+                if algorithm == "all":
+                    oa = alg_df.iloc[0]["oa"]
+                    aa = alg_df.iloc[0]["aa"]
+                    k = alg_df.iloc[0]["k"]
+                    alg_df = pd.DataFrame({'target_size': range(5, 31), 'oa': [oa] * 26, 'aa': [aa] * 26, 'k': [k] * 26})
+                    linestyle = "--"
+                    color = "#000000"
+                    marker = None
+                else:
+                    algorithm_counter = algorithm_counter + 1
+
                 axes[metric_index, ds_index].plot(alg_df['target_size'], alg_df[metric],
-                        label=algorithm_label, marker=markers[algorithm_counter], color=colors[algorithm_counter],
-                        fillstyle='none', markersize=10, linewidth=2
+                        label=algorithm_label, marker=marker, color=color,
+                        fillstyle='none', markersize=10, linewidth=2, linestyle=linestyle
                         )
 
             axes[metric_index, ds_index].set_xlabel('Target size', fontsize=18)
@@ -93,8 +115,8 @@ def plot_saved(exclude=None):
 if __name__ == "__main__":
     #plot_saved()
     plot_oak([
-        "results/v0_summary.csv",
-        "results/v9_summary.csv"
+        "saved_results/v0/v0_summary.csv",
+        "saved_results/all/all_all_features_summary.csv"
     ])
 
 
