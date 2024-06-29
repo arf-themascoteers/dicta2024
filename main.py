c5c388a59635fb885d6b41f06355c87d8f9dd172
aarahman <arif@themascoteers.com>
2024-06-29 22:11:46 +1000

    Ignored

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from task_runner import TaskRunner
 if __name__ == '__main__':
     tag = "all"
     tasks = {
-        "algorithms" : ["pcal","mcuve","bsnet","v0","v4"],
+        "algorithms" : ["pcal","mcuve","bsnet","v0","v1","v2","v3","v4"],
         "datasets" : ["indian_pines","paviaU","salinas"],
         "target_sizes" : list(range(30,4,-1))
     }
