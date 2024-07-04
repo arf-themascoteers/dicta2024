e983decb71f4cbb7cc92110396a064ebca49ce85
Arif <arif@themascoteers.com>
2024-07-04 19:22:22 +1000

    done

--- ds_manager.py
+++ ds_manager.py
@@ -1,125 +1,47 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from data_splits import DataSplits
-import numpy as np
 from sklearn.preprocessing import minmax_scale
 
 
 class DSManager:
-    def __init__(self, name, folds=1, remove_bg=False):
+    def __init__(self, name, remove_bg=False):
         self.name = name
-        self.folds = folds
-        self.init_seed = 40
-        self.random_state_start = 80
-        self._reset_seed()
         dataset_path = f"data/{name}.csv"
         df = pd.read_csv(dataset_path)
-        self.remove_bg = remove_bg
         df.iloc[:, :-1] = minmax_scale(df.iloc[:, :-1])
         self.data = df.to_numpy()
+        print(f"{self.name}: Total samples", len(self.data))
+        self.foreground_data = self.data[self.data[:, -1] != 0]
+        if remove_bg:
+            self.foreground_data[:, -1] = self.foreground_data[:, -1] - 1
+            self.data = self.foreground_data
+        self.bs_train = self.data
+        print(f"{self.name}: After background processing, Total samples", len(self.data))
 
     def get_name(self):
         return self.name
 
-    def count_rows(self):
-        return self.data.shape[0]
+    def get_train_data(self):
+        return self.bs_train
 
-    def count_features(self):
-        return self.data.shape[1]-1
-    
-    def _shuffle(self, seed):
-        self._set_seed(seed)
-        shuffled_indices = np.random.permutation(self.data.shape[0])
-        self._reset_seed()
-        return self.data[shuffled_indices]
+    def get_foreground_data(self):
+        return self.foreground_data
 
     def get_k_folds(self):
-        for i in range(self.folds):
-            seed = self.random_state_start + i
-            yield self.get_all_set_X_y_from_data(seed)
-
-    def get_all_set_X_y_from_data(self, seed):
-        data = self._shuffle(seed)
-        print(f"{self.name}: Total samples", len(data))
-        # data = data[~np.isnan(data).any(axis=1)]
-        # print("Non-NaN samples", len(data))
-        foreground_data = data[data[:, -1] != 0]
-        if self.remove_bg:
-            foreground_data[:, -1] = foreground_data[:, -1] - 1
-            data = foreground_data
-        train = data
-        validation = data
-        print(f"{self.name}: After background processing, Total samples", len(data))
-        evaluation_train, evaluation_test = train_test_split(foreground_data, test_size=0.95, random_state=seed, stratify=foreground_data[:,-1])
-        return DataSplits(self.name, *DSManager.get_X_y_from_data(train),
-                          *DSManager.get_X_y_from_data(validation),
-                          *DSManager.get_X_y_from_data(evaluation_train),
-                          *DSManager.get_X_y_from_data(evaluation_test)
-                          )
-
-    def _set_seed(self, seed):
-        np.random.seed(seed)
-
-    def _reset_seed(self):
-        np.random.seed(self.init_seed)
+        for i in range(20):
+            seed = 40 + i
+            train_x, test_x, train_y, test_y = train_test_split(self.foreground_data[:.0:-1], self.foreground_data[:-1], test_size=0.95, random_state=seed, stratify=self.foreground_data[:,-1])
+            yield DataSplits(self.name, self.bs_train[:,0:-1], self.bs_train[:, -1], train_x, train_y, test_x, test_y)
 
     def __repr__(self):
         return self.get_name()
-
-    @staticmethod
-    def get_X_y_from_data(data):
-        x = data[:, :-1]
-        y = data[:, -1]
-        return x, y
 
     @staticmethod
     def get_dataset_names():
         return [
             "indian_pines",
             "paviaU",
-            "salinasA",
-
-            "pavia",
             "salinas"
         ]
-
-
-if __name__ == "__main__":
-    from collections import Counter
-    df = pd.read_csv("data/indian_pines.csv")
-    size = len(df)
-
-    ds = DSManager("indian_pines")
-    for split in ds.get_k_folds():
-        a = len(split.train_x)
-        b = len(split.validation_x)
-        c = len(split.evaluation_train_x)
-        d = len(split.evaluation_test_x)
-        tot = a + b + c + d
-        print(a,b,c,d,tot,size)
-
-        class_counts = Counter(split.train_y)
-        sorted_class_counts = dict(sorted(class_counts.items()))
-        for k,v in sorted_class_counts.items():
-            print(f"{v:>5}", end="")
-        print("")
-
-        class_counts = Counter(split.validation_y)
-        sorted_class_counts = dict(sorted(class_counts.items()))
-        for k,v in sorted_class_counts.items():
-            print(f"{v:>5}", end="")
-        print("")
-
-        class_counts = Counter(split.evaluation_train_y)
-        sorted_class_counts = dict(sorted(class_counts.items()))
-        for k,v in sorted_class_counts.items():
-            print(f"{v:>5}", end="")
-        print("")
-
-        class_counts = Counter(split.evaluation_test_y)
-        sorted_class_counts = dict(sorted(class_counts.items()))
-        for k,v in sorted_class_counts.items():
-            print(f"{v:>5}", end="")
-
-        print("")
 
