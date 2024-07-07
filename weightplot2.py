aa47763d09cfd0800fe902e307b31df050bf593a
Arif <arif@themascoteers.com>
2024-07-07 16:41:26 +1000

    done

--- weightplot2.py
+++ weightplot2.py
@@ -1,17 +1,33 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 
-#data = pd.read_csv('newest/dummyv0/dummyv0_v0_indian_pines_5_weights.csv').values[:, 1:]
-data = pd.read_csv('newest/dummyv2/dummyv2_v2_indian_pines_5.csv').values[:, 1:]
-means = np.mean(data, axis=1)
-stds = np.std(data, axis=1)
-cv = stds / means
+paths = [
+    'newest/dummyv0/dummyv0_v0_indian_pines_5_weights.csv',
+    'newest/dummyv2/dummyv2_v2_indian_pines_5_weights.csv'
+]
 
-plt.figure(figsize=(10, 6))
-plt.plot(cv, linestyle='-')
-plt.xlabel('Feature Index')
-plt.ylabel('Coefficient of Variation (CV)')
-plt.title('Coefficient of Variation among Features')
+num_paths = len(paths)
+fig, axes = plt.subplots(num_paths, 2, figsize=(15, 6 * num_paths))
+
+if num_paths == 1:
+    axes = [axes]
+
+for i, path in enumerate(paths):
+    data = pd.read_csv(path).values[:, 1:]
+    means = np.mean(data, axis=1)
+    stds = np.std(data, axis=1)
+    cv = stds / means
+
+    axes[i][0].plot(cv, linestyle='-')
+    axes[i][0].set_xlabel('Feature Index')
+    axes[i][0].set_ylabel('Coefficient of Variation (CV)')
+    axes[i][0].set_title(f'Coefficient of Variation (CV) for {path}')
+
+    axes[i][1].plot(means, linestyle='-')
+    axes[i][1].set_xlabel('Feature Index')
+    axes[i][1].set_ylabel('Mean')
+    axes[i][1].set_title(f'Mean for {path}')
+
+plt.tight_layout()
 plt.show()
---
+++ weightplot3.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+paths = {
+    'newest/dummyv0/dummyv0_v0_indian_pines_5_weights.csv' : "BS-Net-Classifier [6]",
+    'newest/dummyv2/dummyv2_v2_indian_pines_5_weights.csv' : "BS-Net-Classifier [6] with early aggregation of weights"
+}
+
+fig, axes = plt.subplots(1, 2, figsize=(15, 6))
+
+for path in paths:
+    data = pd.read_csv(path).values[:, 1:]
+    means = np.mean(data, axis=1)
+    stds = np.std(data, axis=1)
+    cv = stds / means
+
+    axes[0].plot(cv, linestyle='-', label=paths[path])
+    axes[1].plot(means, linestyle='-', label=paths[path])
+
+axes[0].set_xlabel('Epoch')
+axes[0].set_ylabel('Coefficient of Variation (CV)')
+axes[0].set_title('Coefficient of Variation (CV) of weights across the samples')
+axes[0].legend(loc='center left', bbox_to_anchor=(1, 0.5))
+axes[0].legend()
+
+axes[1].set_xlabel('Epoch')
+axes[1].set_ylabel('Mean weight')
+axes[1].set_title('Mean weight across the samples')
+axes[1].legend(loc='center left', bbox_to_anchor=(1, 0.5))
+axes[1].legend()
+
+plt.show()
