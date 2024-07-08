aa8577461194bcd2310b51e8c0f402f0b5585581
Arif <arif@themascoteers.com>
2024-07-09 00:21:09 +1000

    done

--- weight_batch.py
+++ weight_batch.py
@@ -1,19 +1,28 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
-data = pd.read_csv('your_file.csv')
-batch = data.iloc[:, 0]
-mean_weight = data.iloc[:, 1]
-std_weight = data.iloc[:, 2]
+os.makedirs('saved_plots', exist_ok=True)
 
-fig, ax1 = plt.subplots()
+data = pd.read_csv('8_7_weights/v0_weight_all/v0_weight_all_v0_weight_all_indian_pines_5_weights_all.csv')
+data = data.iloc[0:4, :]
+batches = data.iloc[:, 0]
+weights = data.iloc[:, 1:]
 
-ax1.plot(batch, mean_weight, 'b-')
-ax1.set_xlabel('Batch Number')
-ax1.set_ylabel('Mean Weight', color='b')
+fig, ax = plt.subplots()
 
-ax2 = ax1.twinx()
-ax2.plot(batch, std_weight, 'r-')
-ax2.set_ylabel('Standard Deviation of Weights', color='r')
+bar_width = 0.2
+index = range(len(batches))
 
-plt.show()
+for i, weight in enumerate(weights.columns):
+    ax.bar([x + i * bar_width for x in index], weights[weight], bar_width, label=f'Mean weight for band {i+1}')
+
+ax.set_xlabel('Batch')
+ax.set_ylabel('Mean weight')
+ax.set_xticks([x + bar_width for x in index])
+ax.set_xticklabels(batches)
+ax.legend()
+
+plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+plt.savefig('saved_plots/bars.png', bbox_inches='tight')
+plt.close()
