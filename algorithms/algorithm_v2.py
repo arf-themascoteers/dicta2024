9457becd9e15e3bccb47ace2a5626d42a2f1255a
Arif <arif@themascoteers.com>
2024-07-09 15:14:07 +1000

    done

--- algorithms/algorithm_v2.py
+++ algorithms/algorithm_v2.py
@@ -53,7 +53,7 @@ class ZhangNet(nn.Module):
 
 
 class Algorithm_v2(Algorithm):
-    def __init__(self, target_size: int, dataset, tag, reporter, verbose):
+    def __init__(self, target_size:int, dataset, tag, reporter, verbose, test):
         super().__init__(target_size, dataset, tag, reporter, verbose, test)
         self.criterion = torch.nn.CrossEntropyLoss()
         self.class_size = len(np.unique(self.dataset.get_train_y()))
