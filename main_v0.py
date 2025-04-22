85e87e79f4853ec71633e328f87bd0b656e3ba34
Arif <arif@themascoteers.com>
2025-04-23 03:10:28 +1000

    done

--- main_v0.py
+++ main_v0.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
         "datasets": ["indian_pines"],
         "target_sizes" : [30]
     }
-    ev = TaskRunner(tasks,tag,skip_all_bands=True, verbose=False)
+    ev = TaskRunner(tasks,tag,skip_all_bands=True, verbose=True)
     summary, details = ev.evaluate()
